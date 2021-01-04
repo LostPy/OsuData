5b3e37b0cc5b5068e475c4ca4219a48d82690c4f
LostPy <lostmusic.osu@gmail.com>
2021-01-04 21:48:20 +0100

    20200104.4
    Correction __init__ to import path of model and delete __pycache__

--- osuData/bin/__init__.py
+++ osuData/bin/__init__.py
@@ -6,4 +6,4 @@ Author: LostPy
 
 from os import path
 
-save_model_path = path.join(path.dirname(path.abspath(f"./{__name__}.py")), 'bin/save_model.bin')
+save_model_path = path.join(path.dirname(__file__), 'save_model.bin')
